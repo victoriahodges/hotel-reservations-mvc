efeb73ee4ef6b879364295fc23ae9ba88972e1b9
Victoria Hodges <victoriaehodges@gmail.com>
2023-08-11 00:29:30 +0100

    update html templates and static index

--- src/main/java/hotel/reservations/hotel/controllers/HotelController.java
+++ src/main/java/hotel/reservations/hotel/controllers/HotelController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HotelController {
     
-    @GetMapping("/index") // http://localhost:8080/index?name=Victoria
-    public String index(
+    @GetMapping("/hello") // http://localhost:8080/index?name=Victoria
+    public String hello(
         @RequestParam(name="name", required=false, 
                 defaultValue="World") String name, Model model) {
         model.addAttribute("user", name);
-        return "index"; // look for index.html in src/main/resources/templates folder
+        return "hello"; // look for index.html in src/main/resources/templates folder
     }
 }
