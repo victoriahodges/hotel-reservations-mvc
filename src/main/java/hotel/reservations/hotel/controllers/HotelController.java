3754e3852425e29234ac8f942a9119202f380f92
Victoria Hodges <victoriaehodges@gmail.com>
2023-08-11 00:02:21 +0100

    create controller

---
+++ src/main/java/hotel/reservations/hotel/controllers/HotelController.java
@@ -0,0 +1,18 @@
+package hotel.reservations.hotel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HotelController {
+    
+    @GetMapping("/index") // http://localhost:8080/index?name=John
+    public String index(
+        @RequestParam(name="name", required=false, 
+                defaultValue="World") String name, Model model) {
+        model.addAttribute("user", name);
+        return "index"; // look for index.html in src/main/resources/templates folder
+    }
+}
