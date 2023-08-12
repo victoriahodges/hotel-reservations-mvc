70161f9a48925104e3681d8a853634374ad9c749
Victoria Hodges <victoriaehodges@gmail.com>
2023-08-12 12:19:40 +0100

    tests to view what context generates

--- src/test/java/hotel/reservations/hotel/HotelApplicationTests.java
+++ src/test/java/hotel/reservations/hotel/HotelApplicationTests.java
@@ -1,13 +1,32 @@
 package hotel.reservations.hotel;
 
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import hotel.reservations.hotel.json.Greeting;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class HotelApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
+		System.out.println(context.getClass().getName());
+		int count = context.getBeanDefinitionCount();
+		System.out.println("Bean count = " + count);
+	}
+
+	@Test
+	void noGreetingBeanInAppCtx() {
+		assertThrows(NoSuchBeanDefinitionException.class, 
+			() -> context.getBean(Greeting.class));
 	}
 
 }
